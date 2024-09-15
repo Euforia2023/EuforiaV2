b19d228037283857c61e31b16e927f16b3f27718
Euforia2023 <radioeuforia2023@protonmail.com>
2024-09-15 21:01:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,20 @@ const allNavItems = document.querySelectorAll('.nav__item')
 const playerBtnStart = document.querySelector('.fa-circle-play')
 const playerBtnStop = document.querySelector('.fa-stop')
 
+// PLAYER
+const audio = new Audio('https://s3.slotex.pl/shoutcast/7510/stream?nocache=1726423753')
+const btnPlay = document.querySelector('.btn1')
+const btnStop = document.querySelector('.btn2')
+
+const buttonOnclick = () => {
+	btnPlay.addEventListener('click', () => {
+		audio.play()
+	})
+
+	handlePlayersStart()
+}
+// PLAYER
+
 // const closePlayerBox = document.querySelector('.fa-xmark')
 const playerBox = document.querySelector('.player_box')
 
@@ -27,6 +41,10 @@ const handlePlayersStart = () => {
 const handlePlayersStop = () => {
 	playerBtnStart.style.display = 'block'
 	playerBtnStop.style.display = 'none'
+
+	btnStop.addEventListener('click', () => {
+		audio.pause()
+	})
 }
 
 // const handlerPlayerBox = () => {
@@ -49,5 +67,5 @@ const handleNavItemAnimation = () => {
 
 navBtn.addEventListener('click', handlerNav)
 // playerBtn.addEventListener('click', handlerPlayerBox)
-playerBtnStart.addEventListener('click', handlePlayersStart)
+playerBtnStart.addEventListener('click', buttonOnclick)
 playerBtnStop.addEventListener('click', handlePlayersStop)
