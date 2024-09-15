d1ddd0ec8665251dc0dc48f196573c9a9249f90e
Euforia2023 <radioeuforia2023@protonmail.com>
2024-09-15 16:33:21 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
 const allNavItems = document.querySelectorAll('.nav__item')
+const playerBtn = document.querySelector('.fa-circle-play')
+const closePlayerBox = document.querySelector('.fa-xmark')
+const playerBox = document.querySelector('.player_box')
 
 const handlerNav = () => {
 	nav.classList.toggle('nav--active')
@@ -14,14 +17,23 @@ const handlerNav = () => {
 	handleNavItemAnimation()
 }
 
+const handlerPlayerBox = () => {
+	playerBox.classList.add('player_box--active')
+
+	closePlayerBox.addEventListener('click', () => {
+			playerBox.classList.remove('player_box--active')
+		})
+}
+
 const handleNavItemAnimation = () => {
 	let delayTime = 0
 
 	allNavItems.forEach(item => {
 		item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's';
-        delayTime++;
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
 	})
 }
 
 navBtn.addEventListener('click', handlerNav)
+playerBtn.addEventListener('click', handlerPlayerBox)
