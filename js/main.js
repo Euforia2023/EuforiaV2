c6e81a298c201017e252c528ba59273dfe197a9e
Euforia2023 <radioeuforia2023@protonmail.com>
2024-09-26 03:50:09 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@
 	//randomTextWendesday,
 	//randomTextThirsday,
 //} from './components.js'
+import 'jQuety.js'
 
 const nav = document.querySelector('.nav')
 const navBtn = document.querySelector('.burger-btn')
