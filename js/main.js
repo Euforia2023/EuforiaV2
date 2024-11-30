e03f92a02e8cb543fc057808af1a07ea87dca9d0
Euforia2023 <radioeuforia2023@protonmail.com>
2024-11-30 21:34:53 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -157,21 +157,21 @@ const handleCurrentYear = () => {
 
 // setInterval(createSnow, 1000)
 
-const createSnow = () => {
-	const snowflake = document.createElement('i')
-	snowflake.classList.add('snowflake')
-	snowflake.textContent = '❄️'
+// const createSnow = () => {
+// 	const snowflake = document.createElement('i')
+// 	snowflake.classList.add('snowflake')
+// 	snowflake.textContent = '❄️'
 
-	snowflake.style.left = Math.random() * window.innerWidth + 'px'
-	snowflake.style.animationDuration = Math.random() * 5 + 3 + 's'
-	snowflake.style.opacity = Math.random()
+// 	snowflake.style.left = Math.random() * window.innerWidth + 'px'
+// 	snowflake.style.animationDuration = Math.random() * 5 + 3 + 's'
+// 	snowflake.style.opacity = Math.random()
 
-	document.body.append(snowflake)
+// 	document.body.append(snowflake)
 
-	setTimeout(() => {
-		snowflake.remove()
-	}, 8000)
-}
+// 	setTimeout(() => {
+// 		snowflake.remove()
+// 	}, 8000)
+// }
 
 setInterval(createSnow, 2000)
 
